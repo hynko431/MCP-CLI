c3587386c811d9002d075be76fea8a7ec82b448b
hynko431 <sivabhanu7198@gmail.com>
2026-07-01 22:14:34 +0530

    Remove all code files from repository

--- 1342. Number of Steps to Reduce a Number to Zero.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def numberOfSteps(self, num: int) -> int:
-        if num == 0:
-            return 0
-    
-        bits = len(bin(num)) - 2      # remove '0b'
-        ones = bin(num).count('1')
-        
-        return (bits - 1) + ones
